7dde3ffa229ed4604ed523e42eea0c0cd11fc860
Viktoryia Klimenka <candentira@gmail.com>
2018-03-14 23:50:34 +0300

    submit solution

--- src/index.js
+++ src/index.js
@@ -1,44 +1,88 @@
 class SmartCalculator {
 
   constructor(initialValue) {
-    this.stack = [initialValue];
-    this.operators = [];
+    this._stack = [initialValue];
   }
 
   add(number) {
-    this.stack = this.stack.push(number);
-    this.operators.push(this._add);
+    this._stack.push([1, (x, y) => x + y]);
+    this._stack.push(number);
     return this;
   }
   
   subtract(number) {
-    // your implementation
+    this._stack.push([1, (x, y) => x - y]);
+    this._stack.push(number);
+    return this;
   }
 
   multiply(number) {
-    this.stack = number * this.stack;
+    this._stack.push([2, (x, y) => x * y]);
+    this._stack.push(number);
     return this;
   }
 
   devide(number) {
-    this.stack = this.stack / number;
+    this._stack.push([2, (x, y) => x / y]);
+    this._stack.push(number);
     return this;
   }
 
   pow(number) {
-    this.stack = Math.pow(this.stack, number);
+    this._stack.push([3, (x, y) => Math.pow(x, y)]);
+    this._stack.push(number);
     return this;
   }
 
   valueOf() {
-    return this.stack;
+    let expr = this._toPolishNotation(this._stack);
+    return this._calculate(expr);
   }
-  
-  _add = (x, y) => x + y;
-  _subtract = (x, y) => x - y;
-  _multiply = (x, y) => x * y;
-  _devide = (x, y) => x / y;
-  _pow = (x, y) => Math.pow(x, y);
+
+  _toPolishNotation(expression){
+    let polish = [];
+    let operations = [];
+    let lastOperation;
+    let next = expression.shift();
+    while(next) {
+      if(!Array.isArray(next)) {
+        //this is number
+        polish.push(next);
+      } else {
+        //this is operation
+        lastOperation = operations[operations.length - 1];
+        if(lastOperation) {
+          while(next[0] <= lastOperation[0]) {
+            polish.push(operations.pop());
+            if(operations.length > 0) {
+              lastOperation = operations[operations.length - 1];
+            } else {
+              break;
+            }
+          }
+        }
+        operations.push(next);
+      }
+      next = expression.shift();
+    }
+    return polish.concat(operations.reverse());
+  }
+
+  _calculate(expression){
+    let stack = [];
+    let next = expression.shift();
+    while(next) {
+      if(Array.isArray(next)) {
+        let x = stack.pop();
+        let y = stack.pop();
+        stack.push(next[1](y, x));
+      } else {
+        stack.push(next);
+      }
+      next = expression.shift();
+    }
+    return stack[0];
+  }
 }
 
 module.exports = SmartCalculator;
